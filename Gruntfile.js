7eeb1e5b19c39d85c57a8f881d66d7cf8fb45ded
Kyle Weber <kwibbles@gmail.com>
2015-03-30 11:42:42 -0500

    Messing with grunt, trying out connect, watch, and adding other .css files in copy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,11 +21,33 @@ module.exports = function (grunt) {
         }]
       }
     },
+    connect: {
+      build: {
+        options: {
+          port: 2015,
+          base: 'build'
+        }
+      }
+    },
+    watch: {
+      assemble: {
+        files: ['src/partials/**/*.hbs'],
+        tasks: ['assemble'],
+        options: {
+          livereload: true,
+          spawn: false
+        }
+      }
+    },
     copy: {
       build: {
-        files: [{
-          src: 'node_modules/normalize.css/normalize.css',
+        files: [
+        { src: 'node_modules/normalize.css/normalize.css',
           dest: 'build/vendor/normalize.css'
+        },
+        {
+          src: 'node_modules/bootstrap/dist/css/bootstrap.min.css',
+          dest: 'build/vendor/bootstrap.min.css'
         }]
       }
     },
@@ -38,12 +60,16 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('build', ['assemble', 'copy']);
 
   grunt.registerTask('deploy', ['build', 'gh-pages']);
+
+  grunt.registerTask('connect', ['build', 'connect', 'watch', 'gh-pages']);
 
   grunt.registerTask('default', ['build']);
 
